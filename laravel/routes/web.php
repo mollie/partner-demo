0b2fcce954587810f0f6ab04ea77b24eb94ee1d3
Thiago Cordeiro <thiago.cordeiro@mollie.com>
2019-10-15 14:49:26 +0200

    Removed duplications

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -18,11 +18,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
