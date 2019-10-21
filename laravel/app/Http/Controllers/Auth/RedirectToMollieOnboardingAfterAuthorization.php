650fb4c4496fa035e2a096d6607cf1700a0ff611
Martijn Smit <smit@mollie.com>
2019-10-21 16:59:45 +0200

    Add the subit onboarding status call

--- laravel/app/Http/Controllers/Auth/RedirectToMollieOnboardingAfterAuthorization.php
+++ laravel/app/Http/Controllers/Auth/RedirectToMollieOnboardingAfterAuthorization.php
@@ -3,38 +3,49 @@
 
 namespace App\Http\Controllers\Auth;
 
-
-use App\Factories\MollieApiClientFactory;
-use App\MollieStatus;
 use App\Services\AuthenticatedUserLoader;
+use App\Services\Mollie\GetOnboardingStatusService;
+use App\Services\Mollie\SubmitOnboardingDataService;
 
 class RedirectToMollieOnboardingAfterAuthorization
 {
     /**
-     * @var MollieApiClientFactory
+     * @var GetOnboardingStatusService
      */
-    private $apiClientFactory;
+    private $onboardingStatusService;
 
     /**
      * @var AuthenticatedUserLoader
      */
     private $userLoader;
+    /**
+     * @var SubmitOnboardingDataService
+     */
+    private $submitOnboardingDataService;
 
-    public function __construct(MollieApiClientFactory $apiClientFactory, AuthenticatedUserLoader $userLoader)
+    public function __construct(
+        GetOnboardingStatusService $onboardingStatusService,
+        AuthenticatedUserLoader $userLoader,
+        SubmitOnboardingDataService $submitOnboardingDataService
+    )
     {
-        $this->apiClientFactory = $apiClientFactory;
+        $this->onboardingStatusService = $onboardingStatusService;
         $this->userLoader = $userLoader;
+        $this->submitOnboardingDataService = $submitOnboardingDataService;
     }
 
     public function __invoke()
     {
-        $apiClient = $this->apiClientFactory->createForUser($this->userLoader->load());
-        $response = $apiClient->onboarding->get();
+        $user = $this->userLoader->load();
 
-        $mollieStatus = MollieStatus::fromOnboardingApiResponse($response);
+        $mollieStatus = $this->onboardingStatusService->getOnboardingStatus($user);
+
+        if (!$mollieStatus->needsData()) {
+            return redirect(route("payment_settings"));
+        }
+
+        $this->submitOnboardingDataService->submitOnboardingData($user);
 
         return redirect($mollieStatus->getDashboardLink());
     }
-
-
 }
