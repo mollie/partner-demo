36a9da898096fd77fd07ae0896b878d644e8cfdc
Albert Carreras <albert.carreras@mollie.com>
2019-10-21 15:11:33 +0200

    Matching color, welcome copy

--- laravel/resources/views/home/index.blade.php
+++ laravel/resources/views/home/index.blade.php
@@ -1,13 +1,15 @@
-@extends('app', ['title' => __('Welcome'), 'active' => 'home'])
+@extends('app', ['title' => __('Home'), 'active' => 'home'])
 
 @section('dashboard_content')
-    <h1>Welcome to Mollie</h1>
+    <h1>Hello, {{ Auth::user()->company_name }}!</h1>
     <p>
-    Welcome to Mollie's Onboarding, {{ Auth::user()->company_name }}.<br/>
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essen
+        Welcome to <i>Amazing Platform Demo App.</i></p>
+    <p>
+        This is a dummy platform to be used as an example of the Mollie's Hosted Onboarding.
+        To test the example functionality, press the button below or navigate to <strong>Settings</strong> through the sidebar.
     </p>
     <br/>
     <a href="settings/payment">
-        <button class="btn-primary rounded btn-lg">Activate payments</button>
+        <button class="btn-primary rounded btn-lg">Payments setup</button>
     </a>
 @endsection
--- laravel/resources/views/includes/sidebar.blade.php
+++ laravel/resources/views/includes/sidebar.blade.php
@@ -1,10 +1,10 @@
 <!-- sidebar nav -->
 <nav id="sidebar-nav">
     <ul class="nav flex-column nav-pills">
-        <li class="nav-item"><a class="nav-link font-weight-bold  {{ $active === 'home' ? 'active' : ''}}"  href="{{ route('home') }}">{{ __('Home') }}</a></li>
+        <li class="nav-item"><a class="nav-link   {{ $active === 'home' ? 'active' : 'font-weight-bold'}}"  href="{{ route('home') }}">{{ __('Home') }}</a></li>
         <li class="nav-item"><a class="nav-link">Placeholder</a></li>
         <li class="nav-item"><a class="nav-link">Another placeholder</a></li>
         <li class="nav-item"><a class="nav-link">Last Placeholder</a></li>
-        <li class="nav-item font-weight-bold"><a class="nav-link {{ $active === 'settings' ? 'active' : ''}}" href="{{ route('payment_status') }}">{{ __('Settings') }}</a></li>
+        <li class="nav-item"><a class="nav-link {{ $active === 'settings' ? 'active' : 'font-weight-bold'}}" href="{{ route('payment_status') }}">{{ __('Settings') }}</a></li>
     </ul>
 </nav>
