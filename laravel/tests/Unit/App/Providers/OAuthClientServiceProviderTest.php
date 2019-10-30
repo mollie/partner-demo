2e138a812390099fd291ac8f72f748703193815f
Thiago Cordeiro <thiago.cordeiro@mollie.com>
2019-10-30 15:25:28 +0100

    Testing url setup

--- laravel/tests/Unit/App/Providers/OAuthClientServiceProviderTest.php
+++ laravel/tests/Unit/App/Providers/OAuthClientServiceProviderTest.php
@@ -7,10 +7,32 @@ use Tests\TestCase;
 
 class OAuthClientServiceProviderTest extends TestCase
 {
+    const MOLLIE_CUSTOM_API_URL = 'http://api.mollie.nl';
+    const MOLLIE_CUSTOM_WEB_URL = 'http://www.mollie.nl';
+
     public function testEnsureMollieClientIsRegisteredIntoContainer(): void
     {
         $client = $this->app->get(Mollie::class);
 
         $this->assertInstanceOf(Mollie::class, $client);
+    }
+
+    public function testWhenEnvHasDifferentMollieApiUrlThenSetOnClient(): void
+    {
+        $_ENV['MOLLIE_API_URL'] = self::MOLLIE_CUSTOM_API_URL;
+        $_ENV['MOLLIE_WEB_URL'] = self::MOLLIE_CUSTOM_WEB_URL;
+
+        $client = $this->app->get(Mollie::class);
+
+        $expected = new Mollie([
+            'clientId' => $_ENV['MOLLIE_CLIENT_ID'],
+            'clientSecret' => $_ENV['MOLLIE_CLIENT_SECRET'],
+            'redirectUri' => $_ENV['APP_URL'].'/settings/payment/oauth/return',
+            'verify' => 'false',
+        ]);
+
+        $expected->setMollieWebUrl(self::MOLLIE_CUSTOM_WEB_URL);
+        $expected->setMollieApiUrl(self::MOLLIE_CUSTOM_API_URL);
+        $this->assertEquals($expected, $client);
     }
 }
